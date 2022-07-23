45f11dca562332d4ff75da516cbc8548461f59e9
amrik malhans <amrikdeveloper@gmail.com>
2022-07-24 00:40:43 +0530

    refactoring the code making it more readable

--- public/js/popup.js
+++ public/js/popup.js
@@ -1,38 +1,42 @@
 "use strict";
-// Initialize button with user's preferred color
-const connectEl = document.getElementById("connect");
-connectEl.addEventListener("click", async () => {
+// Selecting and declaring all the elements on the page
+const popupConnectBtnEl = document.querySelector(".connect-btn");
+// Add a click listener to the connect button, and execute the content script on the current tab
+popupConnectBtnEl.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        func: connect,
+        target: { tabId: tab.id || 0 },
+        func: connectToPeople, // excute this function in the content script
     });
 });
-async function connect() {
-    const connectBtn = document.querySelectorAll(".entity-result__actions .artdeco-button");
-    // make a mutation observer to wait for the modal to be loaded
+/* connectToPeople is a function that grabs all the 'Connect' buttons on the page
+and clicks them. It waits for the confirmation modal to appear, and clicks the send button
+using MutationObserver. There's a set delay between each 'Connect' button click.
+*/
+const connectToPeople = () => {
+    const linkendInconnectBtnEl = document.querySelectorAll(".entity-result__actions .artdeco-button");
+    const linkendInSendBtnEl = document.querySelector(".artdeco-button.ml1");
+    const linkedInModalEl = document.querySelector("#artdeco-modal-outlet");
+    // make a mutation observer to wait for the modal to be loaded and click the send button
     const observer = new MutationObserver(() => {
-        if (document.querySelector(".artdeco-button.ml1")) {
-            const send = document.querySelectorAll(".artdeco-button.ml1")[0];
-            console.log(send);
-            if (send.innerText === "Send") {
-                send.click();
-            }
+        if (linkendInSendBtnEl) {
+            linkendInSendBtnEl.click();
         }
     });
-    connectBtn.forEach((btn, index) => {
-        // first click the connect button, the wait for the modal to appear, then click the send button
+    // Loop through all the 'Connect' buttons and click them with a delay
+    linkendInconnectBtnEl.forEach((btn, index) => {
+        /* The logic here should be: first click the connect button, then wait
+        for the modal to appear, then click the send button, wait for a set delay */
         if (btn.innerText === "Connect") {
             setTimeout(() => {
                 btn.click();
             }, index * 1000);
         }
-        else {
-            console.log("not connect");
-        }
     });
-    observer.observe(document.querySelector("#artdeco-modal-outlet"), {
+    const config = {
         childList: true,
         subtree: true,
-    });
-}
+    };
+    // Observe the modal on the page
+    observer.observe(linkedInModalEl, config);
+};
--- src/popup.ts
+++ src/popup.ts
@@ -1,49 +1,53 @@
-// Initialize button with user's preferred color
-const connectEl = document.getElementById("connect");
+// Selecting and declaring all the elements on the page
+const popupConnectBtnEl = document.querySelector(".connect-btn") as HTMLElement;
 
-connectEl!.addEventListener("click", async () => {
+// Add a click listener to the connect button, and execute the content script on the current tab
+popupConnectBtnEl.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   chrome.scripting.executeScript({
-    target: { tabId: tab.id! },
-    func: connect,
+    target: { tabId: tab.id || 0 },
+    func: connectToPeople, // excute this function in the content script
   });
 });
 
-async function connect(): Promise<void> {
-  const connectBtn = document.querySelectorAll<HTMLElement>(
+/* connectToPeople is a function that grabs all the 'Connect' buttons on the page
+and clicks them. It waits for the confirmation modal to appear, and clicks the send button
+using MutationObserver. There's a set delay between each 'Connect' button click.
+*/
+const connectToPeople = (): void => {
+  const linkendInconnectBtnEl = document.querySelectorAll<HTMLElement>(
     ".entity-result__actions .artdeco-button"
   );
+  const linkendInSendBtnEl = document.querySelector(
+    ".artdeco-button.ml1"
+  ) as HTMLElement;
+  const linkedInModalEl = document.querySelector(
+    "#artdeco-modal-outlet"
+  ) as HTMLElement;
 
-  // make a mutation observer to wait for the modal to be loaded
+  // make a mutation observer to wait for the modal to be loaded and click the send button
   const observer = new MutationObserver(() => {
-    if (document.querySelector(".artdeco-button.ml1")) {
-      const send = document.querySelectorAll<HTMLElement>(
-        ".artdeco-button.ml1"
-      )[0];
-      console.log(send);
-
-      if (send.innerText === "Send") {
-        send.click();
-      }
+    if (linkendInSendBtnEl) {
+      linkendInSendBtnEl.click();
     }
   });
 
-  connectBtn.forEach((btn, index) => {
-    // first click the connect button, the wait for the modal to appear, then click the send button
+  // Loop through all the 'Connect' buttons and click them with a delay
+  linkendInconnectBtnEl.forEach((btn, index) => {
+    /* The logic here should be: first click the connect button, then wait
+    for the modal to appear, then click the send button, wait for a set delay */
     if (btn.innerText === "Connect") {
       setTimeout(() => {
         btn.click();
       }, index * 1000);
-    } else {
-      console.log("not connect");
     }
   });
 
-  observer.observe(
-    document.querySelector("#artdeco-modal-outlet") as HTMLElement,
-    {
-      childList: true,
-      subtree: true,
-    }
-  );
-}
+  const config = {
+    childList: true,
+    subtree: true,
+  };
+
+  // Observe the modal on the page
+  observer.observe(linkedInModalEl, config);
+};
